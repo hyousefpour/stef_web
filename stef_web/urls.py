2b5a4a47e4bc02fc2a8974f732749176907f15b3
yousefpour <88703538+hyousefpour@users.noreply.github.com>
2021-08-18 19:52:41 +0430

    Update urls.py

--- stef_web/urls.py
+++ stef_web/urls.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('admin/hyousefpour', admin.site.urls),
+#     path('admin/', admin.site.urls),
+    path('admin/hyousefpour', admin.site.urls),
     path('',include('main_page.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('account/', include('account.urls')),
     path('data_prs/', include('prsuser.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
