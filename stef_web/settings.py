b1e45506f434a1a92d0249fa3c5ff845f0872281
yousefpour <88703538+hyousefpour@users.noreply.github.com>
2021-08-18 19:51:15 +0430

    Update settings.py

--- stef_web/settings.py
+++ stef_web/settings.py
@@ -21,9 +21,6 @@ LOGIN_REDIRECT_URL = "account:news"
 LOGIN_URL = "account:login"
 LOGOUT_REDIRECT_URL = "account:login"
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-b_tq%^fp-vqgke6-gcw#5x9-5kaj6+kte&5d9n0i=ur^a7lque'
 
@@ -88,16 +85,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'stef_web.wsgi.application'
 
 DEFAULT_FILE_STORAGE = 'main_page.models.ASCIIFileSystemStorage'
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
